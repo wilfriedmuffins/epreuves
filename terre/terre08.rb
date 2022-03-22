194912d3fdbe49a13917c865b8301a053cf682d6
wilfriedmuffins <wilfried.moffen@live.fr>
2022-03-22 21:21:58 +0100

    terre08.rb done

---
+++ terre/terre08.rb
@@ -0,0 +1,22 @@
+number_1 = ARGV[0].to_i
+number_2 = ARGV[1].to_i
+
+# si string (ARGV[0].to_i == 0 || ARGV[1].to_i == 0) 
+# si négatif (ARGV[0].to_i.negative? || ARGV[1].to_i.negative?)
+# si vide ARGV.empty?
+# si nbr of number supé 2 ARGV.length > 2
+
+# ARGV.each do |arg|
+#     puts arg.match?(/\A-?\d+\Z/) 
+# end
+
+
+if (ARGV[0].to_i.negative? || ARGV[1].to_i.negative?) || ARGV.empty? || ARGV.length > 2 || !ARGV[0].match?(/\A-?\d+\Z/) || !ARGV[1].match?(/\A-?\d+\Z/) 
+    puts "erreur."
+else   
+    puts number_1**number_2
+end
+
+# si arg est string ou arg est neg ou nbre of number sup à 2
+# alors affiché erreur
+# sinon calculer puissance
