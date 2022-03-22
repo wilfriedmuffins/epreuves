3cbc496e47b8fda1980e76f2b321cc7f03c39848
wilfriedmuffins <wilfried.moffen@live.fr>
2022-03-22 15:55:36 +0100

    two methode to invers char terre06.rb done + remove comment

--- terre/terre06.rb
+++ terre/terre06.rb
@@ -1,15 +1,3 @@
-#for i in 0...(ARGV.length)
-
-#puts (ARGV[0].length)
-#puts ARGV.length#[0].length #ARGV[i][ARGV.length-j]
-
-phrase = ARGV
-
-#puts phrase.to_s
-#puts phrase[0][ARGV[0].length-1]
-# puts ARGV[0][ARGV[0].length]
-puts ARGV[0][3]#.length
-puts ARGV[0].length
 invers = ""
 tab = ""
 
@@ -25,21 +13,4 @@ for i in 0...(ARGV[0].length)
     tab+=ARGV[0][ARGV[0].length - 1 - i]
 end
 puts tab
-
-
-
-
-# for i in 0...(ARGV.length)
-#     for j in 0...(ARGV[i].length)
-
-#         puts ARGV[i][ARGV[i].length - 1 - j]
-#         tab.push( ARGV[i][ARGV[i].length - 1 - j])
-#         #puts ARGV[i][ARGV[j].length - j] #ARGV[i][ARGV.length-j]
-#     end
-# end
-
-
-# tab.each do |single|
-#     puts single
-# end
 
