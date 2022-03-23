ff64b092869f390eb8f205dd709b9723c1ce8e2f
wilfriedmuffins <wilfried.moffen@live.fr>
2022-03-23 18:52:07 +0100

    terre07.rb done without length

--- terre/terre07.rb
+++ terre/terre07.rb
@@ -1,6 +1,12 @@
-
+taille=0
 if  ARGV[0] == nil || ARGV.length > 1 || ARGV[0] == "0" || ARGV[0].to_i > 0 
     puts "erreur."
 else 
-    puts ARGV[0].length
+    for i in 1..ARGV[0].length
+        taille+=1
+    end
 end
+
+puts taille
+
+#RECHECK
