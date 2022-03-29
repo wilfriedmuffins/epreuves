ecb831b17d5559c946c50fc1f8ed320f0f1f31d4
wilfriedmuffins <wilfried.moffen@live.fr>
2022-03-29 15:34:56 +0200

    terre15.rb done

---
+++ terre/terre15.rb
@@ -0,0 +1,6 @@
+#Créez un programme qui célèbre votre victoire.
+
+#pseudo_code
+#afficher text "J’ai terminé l’Épreuve de la Terre et c’était [riche en re-reflexion]."
+
+puts  "J’ai terminé l’Épreuve de la Terre et c’était [riche en re-reflexion]."
