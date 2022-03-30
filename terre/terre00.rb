0a62fc5afd69959b8f0980e109fcbd3c1e0ad7fc
wilfriedmuffins <wilfried.moffen@live.fr>
2022-03-30 15:55:01 +0200

    terre00.rb done

--- terre/terre00.rb
+++ terre/terre00.rb
@@ -2,3 +2,11 @@
 for i in (0...1)
     puts "abdcefghijklmnopqrstuvwxyz\n"
 end
+
+alphabet = ["a" "b" "d" "c" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z"]
+
+puts alphabet
+
+alphabet.each do |letter|
+    puts letter
+end
