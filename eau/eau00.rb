075d03ce75ecdc15892bcfa60b2ca799a47692e9
wilfriedmuffins <wilfried.moffen@live.fr>
2022-06-07 18:05:53 +0200

    done eau00.rb

--- eau/eau00.rb
+++ eau/eau00.rb
@@ -16,30 +16,25 @@
 #fonctions utilisées
     number_range = ('000'..'999').to_a
 
-    def unique(tab)
+    def split_element(number_range)
+        number_range.map { |nombre| nombre.split(//).map {|nb| nb.to_i} }
         # tab.map do |nombre|
         #     nombre.split(//).map do |nb|
         #         nb.to_i
         #     end
         # end
-        tab.map { |nombre| nombre.split(//).map {|nb| nb.to_i} }.select { |nombre| (nombre[0] < nombre[1]) && (nombre[1] < nombre[2])}
     end
 
-    def ascending_order(number_unique)
-        number_unique.select { |nombre| (nombre[0] < nombre[1]) && (nombre[1] < nombre[2])}
+    def ascending_order(number_range)
+        number_range.select { |nombre| (nombre[0] < nombre[1]) && (nombre[1] < nombre[2])}
     end
-
-    # Affichage
-    p unique(number_range)
-   
-
-    #test(number_range)
-    # somme(number_unique, number)
-    #no_repeat(number_unique, no_repeat_tab, somme_tab)
-    #puts number_unique
+    
     # Gestion d'érreur
     # Parsing
-    # Résolution
-    # Affichage
 
-   
+    # Résolution
+    split_element(number_range)
+    number_range = ascending_order(number_range)
+
+    # Affichage
+    number_range.each {|element| print " #{element} "}
